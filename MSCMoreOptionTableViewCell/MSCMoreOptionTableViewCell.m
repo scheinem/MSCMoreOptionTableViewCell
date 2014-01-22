c759fc58b4b17f1c8be03ecbe87756c7feed9884
Robert Gilliam <robert@robertgilliam.org>
2014-01-21 22:32:50 -0800

    Add class specific suffix to commonInit.

--- MSCMoreOptionTableViewCell/MSCMoreOptionTableViewCell.m
+++ MSCMoreOptionTableViewCell/MSCMoreOptionTableViewCell.m
@@ -32,7 +32,7 @@
 - (id)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier {
     self = [super initWithStyle:style reuseIdentifier:reuseIdentifier];
     if (self) {
-        [self commonInit];
+        [self commonInitForMSCMoreOptionTableViewCell];
     }
     return self;
 }
@@ -40,12 +40,12 @@
 - (id)initWithCoder:(NSCoder *)aDecoder {
     self = [super initWithCoder:aDecoder];
     if (self) {
-        [self commonInit];
+        [self commonInitForMSCMoreOptionTableViewCell];
     }
     return self;
 }
 
-- (void)commonInit
+- (void)commonInitForMSCMoreOptionTableViewCell
 {
     _configurator = [[MSCMoreOptionTableViewCellConfigurator alloc] initWithCell:self];
     _viewLocator = [[MSCMoreOptionTableViewCellViewLocator alloc] initWithCell:self];
